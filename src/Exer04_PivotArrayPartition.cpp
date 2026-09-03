0f11a41fa9606468b6ffcbbd245c9a362f613385
dgruan <douglasruan27@outlook.com>
2026-09-03 20:53:10 -0300

    Implement array partitioning by pivot value
    The program partitions an array based on a pivot value, collecting elements less than, equal to, and greater than the pivot in separate steps.

--- src/Exer04_PivotArrayPartition.cpp
+++ src/Exer04_PivotArrayPartition.cpp
@@ -1,3 +1,41 @@
-//
-// Created by Junio Moreira on 27/08/26.
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, pivot;
+
+    cin >> n >> pivot;
+
+    vector<int> nums(n);
+    vector<int> resposta;
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] < pivot) {
+            resposta.push_back(nums[i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == pivot) {
+            resposta.push_back(nums[i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] > pivot) {
+            resposta.push_back(nums[i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << resposta[i] << " ";
+    }
+
+    return 0;
+}
