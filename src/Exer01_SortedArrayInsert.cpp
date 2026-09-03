aa6c849c9fd3f74056cf3b6d1296548481430ef2
dgruan <douglasruan27@outlook.com>
2026-09-03 20:51:36 -0300

    Implement sorted array insertion in C++

--- src/Exer01_SortedArrayInsert.cpp
+++ src/Exer01_SortedArrayInsert.cpp
@@ -1,3 +1,37 @@
-//
-// Created by Junio Moreira on 27/08/26.
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, x;
+
+    cin >> n >> x;
+
+    vector<int> lista(n + 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> lista[i];
+    }
+
+    int posicao = n;
+
+    for (int i = 0; i < n; i++) {
+        if (x < lista[i]) {
+            posicao = i;
+            break;
+        }
+    }
+
+    for (int i = n; i > posicao; i--) {
+        lista[i] = lista[i - 1];
+    }
+
+    lista[posicao] = x;
+
+    for (int i = 0; i < n + 1; i++) {
+        cout << lista[i] << " ";
+    }
+
+    return 0;
+}
