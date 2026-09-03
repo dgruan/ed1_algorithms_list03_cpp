8717086a2472a9d0aad8668a3c6a4f89865b42c5
dgruan <douglasruan27@outlook.com>
2026-09-03 20:52:48 -0300

    Implement merge of two sorted arrays

--- src/Exer03_MergeSortedArrays.cpp
+++ src/Exer03_MergeSortedArrays.cpp
@@ -1,3 +1,51 @@
-//
-// Created by Junio Moreira on 27/08/26.
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<int> a(n);
+    vector<int> b(m);
+    vector<int> resposta;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    int i = 0;
+    int j = 0;
+
+    while (i < n && j < m) {
+        if (a[i] <= b[j]) {
+            resposta.push_back(a[i]);
+            i++;
+        } else {
+            resposta.push_back(b[j]);
+            j++;
+        }
+    }
+
+    while (i < n) {
+        resposta.push_back(a[i]);
+        i++;
+    }
+
+    while (j < m) {
+        resposta.push_back(b[j]);
+        j++;
+    }
+
+    for (int i = 0; i < n + m; i++) {
+        cout << resposta[i] << " ";
+    }
+
+    return 0;
+}
