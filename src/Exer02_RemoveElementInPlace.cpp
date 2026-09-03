133575c1a2fe9352b9d521bd3eec529fca8e0b1b
dgruan <douglasruan27@outlook.com>
2026-09-03 20:52:13 -0300

    Add in-place element removal logic in C++
    Implement in-place removal of specified element from vector.

--- src/Exer02_RemoveElementInPlace.cpp
+++ src/Exer02_RemoveElementInPlace.cpp
@@ -1,3 +1,31 @@
-//
-// Created by Junio Moreira on 27/08/26.
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, val, k = 0;
+
+    cin >> n >> val;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    cout << k << endl;
+
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
